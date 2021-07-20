db6620302f718eb03eda1a99ec40436a919e1920
roshanpatro4177 <roshanrpatro@gmail.com>
2021-07-20 12:13:51 +0530

    First commit

---
+++ springboot-docker-aws-ecs/src/test/java/com/learnwithroshan/aws/ecs/SpringbootDockerAwsEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithroshan.aws.ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerAwsEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
